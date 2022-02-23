fe0e3317361b74f7f7a2e8b8003a965a6b16a32d
cryptoboid <cryptoboid@protonmail.com>
2022-02-23 18:13:01 -0300

    changed gun store imports again

--- src/lib/_modules/stores.ts
+++ src/lib/_modules/stores.ts
@@ -1,8 +1,6 @@
-import Gun from 'gun';
-import type { IGunChainReference } from 'gun/types/chain';
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
 export const secretKey = writable('');
-export const groupDB: Writable<IGunChainReference | undefined> = writable(undefined);
+export const groupDB: Writable<any> = writable(undefined);
 export const groupStore = writable({ expenses: {}, payments: {}, members: {}, groupInfo: { name: 'loading...' } });
